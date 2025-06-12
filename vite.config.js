b2ec86e856133921f4c5aef1001483ff0424927e
Marat Frost <morozovmarat4@gmail.com>
2025-06-12 03:21:28 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: "https://maratffrost.github.io/CV-Vue/cv"
+  base: "https://maratffrost.github.io/CV-Vue"
 })
